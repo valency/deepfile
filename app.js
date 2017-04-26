e060b1b37967c3efc2014e81ef39aff4f8bedf38
Ye Ding <yding@deepera.com>
2017-04-26 18:07:32 +0800

    v0.4.26a [U] Move from ECMA 6 to 5.1

--- app.js
+++ app.js
@@ -9,35 +9,36 @@ const fs = require('fs');
 const path = require('path');
 const util = require('util');
 
-let dir = process.cwd();
-let app = express();
+var dir = process.cwd();
+var app = express();
 app.use(express.static(dir));
 
 
-function log(msg, color = colors.green) {
+function log(msg, color) {
+    if (color === undefined || color === null) color = colors.green;
     console.log(color('[' + moment().format('YYYY-MM-DD HH:mm:ss') + '] ') + msg);
 }
 
 app.post('/', function (req, res) {
     log('Handling post request...');
-    let form = new formidable.IncomingForm();
+    var form = new formidable.IncomingForm();
     form.keepExtensions = true;
     form.multiples = true;
     form.uploadDir = path.join(dir, moment().format('YYYY-MM-DD'));
     fs.mkdir(form.uploadDir, function () {
         form.parse(req, function (err, fields, files) {
-            let f = files[''];
+            var f = files[''];
             if (f === undefined) f = files['files[]'];
             if (f === undefined) f = files['files'];
             if (f === undefined) f = files['file'];
             if (f) {
                 if (f.constructor !== Array) f = [f];
-                for (let i = 0; i < f.length; i++) {
+                for (var i = 0; i < f.length; i++) {
                     f[i]['path'] = f[i]['path'].replace(dir, '').replace(/\\/g, '/');
                 }
                 res.json(f);
             } else {
-                log('Error while handling post request.');
+                log('Error while handling post request.', colors.red);
                 res.status(500).send();
             }
         });
