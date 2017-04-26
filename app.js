9e1fc2617db430bba67f30d7ecd794044c5ed0ae
Ye Ding <yding@deepera.com>
2017-04-26 17:56:55 +0800

    v0.4.26 [F] Fixed bugs when posting with form names

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
 #!/usr/bin/env node
 
-var express = require('express');
-var colors = require('colors/safe');
-var moment = require('moment');
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
-var path = require('path');
-var util = require('util');
+const express = require('express');
+const colors = require('colors/safe');
+const moment = require('moment');
+const http = require('http');
+const formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
 
-var dir = process.cwd();
-var app = express();
+let dir = process.cwd();
+let app = express();
 app.use(express.static(dir));
 
 
-function log(msg) {
-    console.log(colors.green('[' + moment().format('YYYY-MM-DD HH:mm:ss') + '] ') + msg);
+function log(msg, color = colors.green) {
+    console.log(color('[' + moment().format('YYYY-MM-DD HH:mm:ss') + '] ') + msg);
 }
 
 app.post('/', function (req, res) {
     log('Handling post request...');
-    var form = new formidable.IncomingForm();
+    let form = new formidable.IncomingForm();
     form.keepExtensions = true;
     form.multiples = true;
     form.uploadDir = path.join(dir, moment().format('YYYY-MM-DD'));
     fs.mkdir(form.uploadDir, function () {
         form.parse(req, function (err, fields, files) {
-            files = files[''];
-            if (files.constructor !== Array) files = [files];
-            for (var i = 0; i < files.length; i++) {
-                files[i]['path'] = files[i]['path'].replace(dir, '').replace(/\\/g, '/');
+            let f = files[''];
+            if (f === undefined) f = files['files[]'];
+            if (f === undefined) f = files['files'];
+            if (f === undefined) f = files['file'];
+            if (f) {
+                if (f.constructor !== Array) f = [f];
+                for (let i = 0; i < f.length; i++) {
+                    f[i]['path'] = f[i]['path'].replace(dir, '').replace(/\\/g, '/');
+                }
+                res.json(f);
+            } else {
+                log('Error while handling post request.');
+                res.status(500).send();
             }
-            res.json(files);
         });
     });
 });
 
 app.listen(9007, function () {
-    log('Server started.');
+    log('Server listening on port 9007.');
 });
